559a2a795d83fad55bb4e6b7dbcdc7fff82a0992
Tyler Berrett <btylerberrett@gmail.com>
2019-10-18 13:06:48 -0600

    bug fix of crash when setting custom size of matrix's

--- app/src/main/java/com/example/advancedkotlinsprint/Matrix.kt
+++ app/src/main/java/com/example/advancedkotlinsprint/Matrix.kt
@@ -42,7 +42,7 @@ operator fun Array<Array<Int>>.times(a: Array<Array<Int>>): ArrayList<String> {
     val product = Array(row1) {IntArray(column2)}
     for (i in 0 until row1){
         for (j in 0 until column2) {
-            for (k in 0 until column1){
+            for (k in 0 until row1){
                     product[i][j] += this[i][k] * a[k][j]
             }
         }
--- app/src/main/java/com/example/advancedkotlinsprint/controllers/RootController.kt
+++ app/src/main/java/com/example/advancedkotlinsprint/controllers/RootController.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.bluelinelabs.conductor.Controller
 import com.bluelinelabs.conductor.RouterTransaction
 import com.bluelinelabs.conductor.changehandler.HorizontalChangeHandler
@@ -21,32 +22,43 @@ class RootController: Controller {
         val view = inflater.inflate(R.layout.root_controller, container, false)
 
         view.calculate_button.setOnClickListener {
-            Matrix.height = 2
-            Matrix.width = 2
+            Matrix.height = 3
+            Matrix.width2 = 3
+            Matrix.width = 3
+            Matrix.height2 = 3
             val matrix = matrix(Matrix.height, Matrix.width)
+
 
             matrix.set(0,0, view.et_top_left_top.getNumber())
             matrix[0][1] = view.et_top_right_top.getNumber()
             matrix[1][0] = view.et_top_left_bottom.getNumber()
             matrix[1][1] = view.et_top_right_bottom.getNumber()
 
+            matrix.forEach {
+                it.forEach {
+                    println(it)
+                }
+            }
 
-            val matrix2 = Matrix.matrix2
+
+            val matrix2 = matrix(Matrix.height2, Matrix.width2)
 
             matrix2[0][0] = view.et_bottom_left_top.getNumber()
             matrix2[0][1] = view.et_bottom_right_top.getNumber()
             matrix2[1][0] = view.et_bottom_left_bottom.getNumber()
             matrix2[1][1] = view.et_bottom_right_bottom.getNumber()
 
-
-            val finalProduct = matrix.times(matrix2)
-            args.putStringArrayList(MainActivity.BUNDLE_KEY, finalProduct)
-
-
-            router.pushController(RouterTransaction.with(ChildController(args))
-                .pushChangeHandler(HorizontalChangeHandler())
-                .popChangeHandler(HorizontalChangeHandler())
-            )
+            if (Matrix.width == Matrix.height2){
+                val finalProduct = matrix.times(matrix2)
+                args.putStringArrayList(MainActivity.BUNDLE_KEY, finalProduct)
+                router.pushController(RouterTransaction.with(ChildController(args))
+                    .pushChangeHandler(HorizontalChangeHandler())
+                    .popChangeHandler(HorizontalChangeHandler())
+                )
+            } else {
+                val alert = "The number of columns in the first matrix should be equal to the number of rows in the second."
+                Toast.makeText(view.context, alert, Toast.LENGTH_LONG).show()
+            }
         }
 
         return view
